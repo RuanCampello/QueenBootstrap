d30b0a2b4c32032fa675975ac0374013749e0660
RuanCampello <ruancampello888@gmail.com>
2023-06-15 18:35:49 -0300

    new file:   audio/Queen - Don't Stop Me Now (Official Video) (64 kbps).mp3 	new file:   "audio/Queen \342\200\223 Bohemian Rhapsody (Official Video Remastered) (64 kbps).mp3" 	new file:   img/bohemianrhapsody.jpg 	new file:   img/doodle_Freddie.jpg 	new file:   img/killerqueen.JPG 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 let playbutton = document.getElementById("playbutton");
 let pausebutton = document.getElementById("pausebutton");
-let time = document.getElementById('tracktime');
+let currentAudioTime = document.getElementById('currentAudioTime');
+let totalAudioDuration = document.getElementById('totalAudioDuration');
+let currentAudioTime1 = document.getElementById('currentAudioTime1');
+let totalAudioDuration1 = document.getElementById('totalAudioDuration1');
+let currentAudioTime2 = document.getElementById('currentAudioTime2');
+let totalAudioDuration2 = document.getElementById('totalAudioDuration2');
 
 var audio = document.getElementById("audio");
-function playAudio() {
-	audio.play();
+var audio1 = document.getElementById("audio1");
+var audio2 = document.getElementById("audio2");
+
+function playAudio(track) {
+	if(track == 0){
+		audio.play();
+	}
+	else if(track == 1){
+		audio1.play();
+	}
+	else if(track == 2){
+		audio2.play();
+	}
 }
-function pauseAudio() {
-	audio.pause();
+function pauseAudio(track) {
+	if(track == 0){
+		audio.pause();
+	}
+	else if(track == 1){
+		audio1.pause();
+	}
+	else if(track == 2){
+		audio2.pause();
+	}
 }
-function updateTime(track) {
-		time.innerHTML =
-		Math.floor((track.currentTime)/60) + ':'+Math.floor((track.currentTime)%60) + '/' + Math.floor((track.duration)/60) +':'+ Math.floor((track.duration)%60);
+
+function update_time(current_time, audio_duration, song){
+	if(song == 0){
+		currentAudioTime.innerHTML = (format_time(current_time));
+		totalAudioDuration.innerHTML = (format_time(audio_duration));
+	}
+  else if(song == 1){
+		currentAudioTime1.innerHTML = (format_time(current_time));
+		totalAudioDuration1.innerHTML = (format_time(audio_duration));
+	}
+	else if(song == 2){
+		currentAudioTime2.innerHTML = (format_time(current_time));
+		totalAudioDuration2.innerHTML = (format_time(audio_duration));
+	}
+}
+function format_time(audio_duration){
+  sec = Math.floor( audio_duration );
+  min = Math.floor( sec / 60 );
+  min = min >= 10 ? min : '0' + min;
+  sec = Math.floor( sec % 60 );
+  sec = sec >= 10 ? sec : '0' + sec;
+  return min + ":"+ sec;
 }
